f9d98984a4c371fc8dbfce1f8a53da9fba1b6ef3
nlloydw <nathan@revolutiongrowth.co.uk>
2022-07-13 19:50:09 +0100

    completed it

--- public/js/site.js
+++ public/js/site.js
@@ -17457,7 +17457,7 @@ var _hoisted_1 = {
   "class": "greetings"
 };
 var _hoisted_2 = {
-  "class": "green text-center pl-8"
+  "class": "green"
 };
 
 var _hoisted_3 = /*#__PURE__*/_withScopeId(function () {
@@ -17950,7 +17950,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _HelloWorld_vue_vue_type_template_id_0a4b80a8_scoped_true__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./HelloWorld.vue?vue&type=template&id=0a4b80a8&scoped=true */ "./src/js/components/HelloWorld.vue?vue&type=template&id=0a4b80a8&scoped=true");
 /* harmony import */ var _HelloWorld_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./HelloWorld.vue?vue&type=script&lang=js */ "./src/js/components/HelloWorld.vue?vue&type=script&lang=js");
 /* harmony import */ var _HelloWorld_vue_vue_type_style_index_0_id_0a4b80a8_scoped_true_lang_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./HelloWorld.vue?vue&type=style&index=0&id=0a4b80a8&scoped=true&lang=css */ "./src/js/components/HelloWorld.vue?vue&type=style&index=0&id=0a4b80a8&scoped=true&lang=css");
-/* harmony import */ var _Users_n_williams_Projects_vue_tailwind_base_project_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
+/* harmony import */ var _Users_n_williams_Projects_social_media_dashboard_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./node_modules/vue-loader/dist/exportHelper.js */ "./node_modules/vue-loader/dist/exportHelper.js");
 
 
 
@@ -17958,7 +17958,7 @@ __webpack_require__.r(__webpack_exports__);
 ;
 
 
-const __exports__ = /*#__PURE__*/(0,_Users_n_williams_Projects_vue_tailwind_base_project_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__["default"])(_HelloWorld_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_HelloWorld_vue_vue_type_template_id_0a4b80a8_scoped_true__WEBPACK_IMPORTED_MODULE_0__.render],['__scopeId',"data-v-0a4b80a8"],['__file',"src/js/components/HelloWorld.vue"]])
+const __exports__ = /*#__PURE__*/(0,_Users_n_williams_Projects_social_media_dashboard_node_modules_vue_loader_dist_exportHelper_js__WEBPACK_IMPORTED_MODULE_3__["default"])(_HelloWorld_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"], [['render',_HelloWorld_vue_vue_type_template_id_0a4b80a8_scoped_true__WEBPACK_IMPORTED_MODULE_0__.render],['__scopeId',"data-v-0a4b80a8"],['__file',"src/js/components/HelloWorld.vue"]])
 /* hot reload */
 if (false) {}
 
